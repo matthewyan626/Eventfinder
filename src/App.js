8ae02c50fa5c0f7371a9578e08ed54ea7c1906c2
mpyan123 <mpyan@usc.edu>
2024-07-06 15:37:39 -0700

    adding files

--- src/App.js
+++ src/App.js
@@ -1,25 +1,21 @@
-import logo from './logo.svg';
-import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css'
+import 'react-bootstrap-typeahead/css/Typeahead.css'
+import SearchPage from './pages/SearchPage'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import FavoritesPage from './pages/FavoritesPage'
+
+
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
 
+
+return (
+   <BrowserRouter>
+        <Routes>
+          <Route path='/search' element={<SearchPage />} />
+          <Route path='/favorites' element={<FavoritesPage />} />
+        </Routes>
+   </BrowserRouter>
+  )
+}
 export default App;
